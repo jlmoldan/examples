d964cbd343a0c5e0480db7ba2ef9f7bb1e490a6b
Jason Moldan <jlmoldan@stthomas.edu>
2017-09-23 01:01:49 +0000

    script command

---
+++ jlmoldan-Lab-02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+
+
+#make directories
+mkdir A C D A/B A/C A/D C/E C/F D/G D/H D/I D/H/J D/H/K
+touch A/aa C/cc D/dd A/B/bb A/C/cc A/D/dd C/E/ee C/F/ff D/G/gg D/H/hh D/I/ii D/H/J/jj D/H/K/kk
+
+# install tree
+apt-get install -y tree
+#insall git
+apt-get install -y git
